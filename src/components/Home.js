85fe69d75dc1376daf9d70a56e659405924d13ea
Kieran Dunch <dunch.kieran@gmail.com>
2023-10-07 15:00:07 -0600

    save bootstrap

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="card">
+        <div class="card-title">Home</div>
+        <p className="card-text">Testing bootstrap</p>
+      </div>
+    </>
+  )
+}
