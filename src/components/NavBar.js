2b38e0e40798a6b5600ce13d9231ef95be489f95
Kieran Dunch <dunch.kieran@gmail.com>
2023-10-08 16:15:58 -0600

    navbar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,11 +7,6 @@ export default function NavBar() {
         <div class="container-fluid">
           <Link to="/" class="navbar-brand text-light" href="">Celestial Journeys</Link>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="btn btn-outline-primary" aria-current="page" href="#">Login</a>
-              </li>
-            </ul>
             <form class="d-flex" role="search">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button class="btn btn-outline-success" type="submit">Search</button>
